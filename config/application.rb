e6cd34cfea7212dc838052c7d54e348935ed4609
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-09-12 12:58:08 +0900

    Initialize repository

---
+++ config/application.rb
@@ -0,0 +1,25 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module TodoApp
+  class Application < Rails::Application
+    config.load_defaults 6.0
+
+    config.time_zone = 'Tokyo'
+    config.i18n.load_path +=
+      Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controller_specs false
+      g.view_specs false
+    end
+  end
+end
