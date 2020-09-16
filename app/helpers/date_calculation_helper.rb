baf4c022baa4f1a5d30bd5f3ac38728829e1395a
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-09-16 16:35:53 +0900

    Install bootstrap4

--- app/helpers/date_calculation_helper.rb
+++ app/helpers/date_calculation_helper.rb
@@ -2,7 +2,7 @@ require 'date'
 
 module DateCalculationHelper
   def get_the_date
-    the_date = Date.parse(request.path.gsub('/', '')).strftime('%Y年%1m月%d日')
+    the_date = Date.parse(request.path.gsub('/', '')).strftime('%Y年%1m月%1d日')
   end
 
   def set_previous_date
--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,12 +1,11 @@
-<template>
-  <div>
-    <b-button variant="primary">Primary</b-button>
-    <b-button variant="secondary">Secondary</b-button>
-    <b-button variant="success">Success</b-button>
-    <b-button variant="danger">Danger</b-button>
-    <b-button variant="warning">Warning</b-button>
-    <b-button variant="info">Info</b-button>
-    <b-button variant="light">Light</b-button>
-    <b-button variant="dark">Dark</b-button>
-  </div>
-</template>
+<template> </template>
+
+<script>
+import Navigation from "./component/navigation.vue";
+
+export default {
+  components: {
+    Navigation
+  }
+};
+</script>
