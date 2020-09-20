a1260d94011cf1eba17925936df150d8fceaff04
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-09-20 14:48:49 +0900

    Add seed data for production

---
+++ db/seeds/production/tasks.rb
@@ -0,0 +1,22 @@
+titles = %w[
+  燃えるゴミを捨てる
+  掃除機をかける
+  子供を迎えにいく
+  オンライン英会話
+  読書
+  ジム
+  Railsの勉強
+  JavaScriptの勉強
+  ウェブ制作案件
+  親に電話をかける
+]
+
+50.times do |n|
+  Task.create!(
+    title: (titles[n % titles.length]).to_s,
+    body: n.even? ? 'メモを残すことができます' : nil,
+    status: n % 3 != 0 ? 'uncompleted' : 'completed',
+    user_id: n % 3 != 0 ? n % 3 : 1,
+    date_id: (Date.today - n.days).to_s
+  )
+end
