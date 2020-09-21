9b4fb802c6b9ccd278d37db21bf3686c7e316428
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-09-22 07:08:26 +0900

    Update task seed data

--- db/seeds/development/tasks.rb
+++ db/seeds/development/tasks.rb
@@ -15,8 +15,7 @@ titles = %w[
   Task.create!(
     title: (titles[n % titles.length]).to_s,
     body: n.even? ? 'メモを残すことができます' : nil,
-    status: false,
     user_id: 1,
-    date_id: (Date.today - (n / 5).days).to_s
+    date_id: (Date.today - (n % 5).days).to_s
   )
 end
