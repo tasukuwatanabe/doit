91e51e40c929245a09a0ae66ca25d442776121da
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-09-26 18:57:50 +0900

    Fix redirect path after user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       flash[:success] = 'ログインしました。'
-      redirect_back_or user
+      redirect_back_or root_path
     else
       flash.now[:danger] = 'メールアドレスもしくはパスワードが正しくありません。'
       render 'new'
