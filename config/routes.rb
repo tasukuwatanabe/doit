89ac788085f7466b61d3e06d651db23933517043
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-09-28 15:48:57 +0900

    Add seed data for routine

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'top#dashboard'
-  get '/dates/:date' => 'todos#index', as: 'index'
-  post '/dates/:date/create_todo/:id', to: 'shortcuts#create_todo', as: 'shortcut_create_todo'
+  get '/date/:date' => 'todos#index', as: 'index'
+  post '/date/:date/create_todo/:id', to: 'shortcuts#create_todo', as: 'shortcut_create_todo'
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
--- db/seeds/development/todos.rb
+++ db/seeds/development/todos.rb
@@ -8,14 +8,28 @@ titles = %w[
   Railsの勉強
   JavaScriptの勉強
   ウェブ制作案件
+  PHPを勉強する
+  Rubyを勉強する
+  Pythonを勉強する
+  Goを勉強する
+  AWSを勉強する
   親に電話をかける
 ]
 
-10.times do |n|
+titles.length.times do |n|
+  case n % 3
+  when 0
+    todo_date = Date.today.yesterday
+  when 1
+    todo_date = Date.today
+  when 2
+    todo_date = Date.today.tomorrow
+  end
+
   Todo.create!(
-    title: (titles[n % titles.length]).to_s,
+    title: titles[n],
     body: n.even? ? 'メモを残すことができます' : nil,
     user_id: User.find_by(email: 'testuser1@example.com').id,
-    todo_date: Date.today
+    todo_date: todo_date
   )
 end
