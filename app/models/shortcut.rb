1a4f754167ae98282962df58d91db91a7a74eb96
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-09-30 21:27:23 +0900

    Add rspec test for shortcut

--- app/models/shortcut.rb
+++ app/models/shortcut.rb
@@ -1,23 +1,24 @@
 class Shortcut < ApplicationRecord
   include StringNormalizer
 
+  MAX_SHORTCUT_COUNT = 10
+
   belongs_to :user
 
   before_validation do
     self.title = normalize_as_text(title)
   end
 
+  validate :shortcut_counts_must_be_within_limit
   validates :title, presence: true, uniqueness: {
     message: '同じタイトルのショートカットが存在します'
   }
 
-  MAX_SHORTCUT_COUNT = 10
+  default_scope -> { order(created_at: :desc) }
 
-  validate do
-    if user.shortcuts.count > MAX_SHORTCUT_COUNT
+  private def shortcut_counts_must_be_within_limit
+    if user.shortcuts.count >= MAX_SHORTCUT_COUNT
       errors.add(:base, "ショートカットが登録できるのは#{MAX_SHORTCUT_COUNT}個までです")
     end
   end
-
-  default_scope -> { order(created_at: :desc) }
 end
