2be0ac625f7654620590802ed0c45fe188ec25b2
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-10-11 15:54:55 +0900

    Fix env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,12 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: AWS_ACCESS_KEY_ID,
-      aws_secret_access_key: AWS_SECRET_KEY_ID,
-      region: AWS_REGION
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_KEY_ID'],
+      region: ENV['AWS_REGION']
     }
 
-    config.fog_directory = S3_DIRECTORY
+    config.fog_directory = ENV['S3_DIRECTORY']
     config.cache_storage = :fog
   end
 end
