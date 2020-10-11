68536f38040536871d59e87a07b3b13d8eb1e1a2
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-10-11 15:40:33 +0900

    Fix unicorn setting

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,7 +1,7 @@
 # set lets
 $worker  = 2
 $timeout = 30
-$app_dir = "/var/www/rails/DoIT"
+$app_dir = '/var/www/rails/doit'
 $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
 $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
@@ -16,17 +16,17 @@ pid $pid
 # loading booster
 preload_app true
 # before starting processes
-before_fork do |server, worker|
+before_fork do |server, _worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
   old_pid = "#{server.config[:pid]}.oldbin"
   if old_pid != server.pid
     begin
-      Process.kill "QUIT", File.read(old_pid).to_i
+      Process.kill 'QUIT', File.read(old_pid).to_i
     rescue Errno::ENOENT, Errno::ESRCH
     end
   end
 end
 # after finishing processes
-after_fork do |server, worker|
+after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
