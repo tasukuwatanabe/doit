829a7a7af31ac98caf6e962ae4068a7cdd765556
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-10-11 19:15:49 +0900

    Add user default image and env file

--- app/helpers/user_image_helper.rb
+++ app/helpers/user_image_helper.rb
@@ -1,6 +1,14 @@
 module UserImageHelper
   def user_image(user)
-    image_path = user.user_image? ? user.user_image.to_s : '/user_images/default.jpg'
+    image_path = if user.user_image?
+                   user.user_image.to_s
+                 else
+                   if Rails.env.production?
+                     'https://doit-image.s3-ap-northeast-1.amazonaws.com/user_images/default.jpg'
+                   else
+                     '/user_images/default.jpg'
+                   end
+                 end
     image_tag(image_path, alt: user.username, class: 'user-icon')
   end
 end
