b8115557d8c82f554863e043801b68681bde2602
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-10-14 23:07:32 +0900

    Fix cron schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,6 +6,6 @@ set :env_path,    '"$HOME/.rbenv/shims":"$HOME/.rbenv/bin"'
 
 job_type :runner, %q{ cd :path && PATH=:env_path:"$PATH" bin/rails runner -e :environment ':task' :output }
 
-every 1.minute do
+every 2.minute do
   runner 'Batch::Guest.destroy'
 end
