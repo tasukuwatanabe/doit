853e1938c28cbf5e4ecbcac1811376275011fc8a
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-10-15 08:23:48 +0900

    Fix carrierwave settings when no image is set

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,12 +21,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url(*_args)
+    #   # For Rails 3.1+ asset pipeline compatibility:
+    #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    #
+    if Rails.env.production?
+      'https://doit-image.s3-ap-northeast-1.amazonaws.com/user_images/default.jpg'
+    else
+      '/user_images/' + [version_name, 'default.jpg'].compact.join('_')
+    end
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
@@ -49,6 +53,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    "user_image_#{model.id}.jpg"
+    "user_image_#{model.id}.jpg" if original_filename
   end
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -6,6 +6,6 @@ set :env_path,    '"$HOME/.rbenv/shims":"$HOME/.rbenv/bin"'
 
 job_type :runner, %q{ cd :path && PATH=:env_path:"$PATH" bin/rails runner -e :environment ':task' :output }
 
-every 2.minute do
+every 1.day, at: '3:30 am' do
   runner 'Batch::Guest.destroy'
 end
