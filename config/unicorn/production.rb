586689b010174929f1c3eda57f26175fd6b8d2e7
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-10-18 19:49:31 +0900

    Delete unicorn.conf file

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,41 +1,37 @@
-# ワーカーの数。後述
-$worker = 2
-# 何秒経過すればワーカーを削除するのかを決める
-$timeout = 30
-# 自分のアプリケーション名、currentがつくことに注意。
-$app_dir = '/var/www/rails/doit/current'
-# リクエストを受け取るポート番号を指定。後述
-$listen = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
-# PIDの管理ファイルディレクトリ
-$pid = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-# エラーログを吐き出すファイルのディレクトリ
-$std_log = File.expand_path 'log/unicorn.log', $app_dir
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
+timeout 15
+preload_app true # 更新時ダウンタイム無し
 
-# 上記で設定したものが適応されるよう定義
-worker_processes  $worker
-working_directory $app_dir
-stderr_path $std_log
-stdout_path $std_log
-timeout $timeout
-listen  $listen
-pid $pid
+app_path = '/var/www/rails/doit'
+app_shared_path = "#{app_path}/shared"
+working_directory "#{app_path}/current/"
 
-# ホットデプロイをするかしないかを設定
-preload_app true
+listen "#{app_shared_path}/tmp/sockets/unicorn.sock"
 
-# fork前に行うことを定義。後述
-before_fork do |server, _worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if old_pid != server.pid
-    begin
-      Process.kill 'QUIT', File.read(old_pid).to_i
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
+stdout_path "#{app_shared_path}/log/unicorn.stdout.log"
+stderr_path "#{app_shared_path}/log/unicorn.stderr.log"
+
+pid "#{app_shared_path}/tmp/pids/unicorn.pid"
+
+# ログの出力
+stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+
+before_fork do |_server, _worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
   end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
 end
 
-# fork後に行うことを定義。後述
 after_fork do |_server, _worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
 end
