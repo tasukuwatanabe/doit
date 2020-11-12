42f24cb1a2433541699729b3c42bc6d8921f60c5
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-11-12 16:11:29 +0900

    Todoの日付ロジックの変更に伴い、specファイルを修正、削除。

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -2,9 +2,6 @@ FactoryBot.define do
   factory :todo do
     title { 'todoタイトル' }
     todo_date { Date.today }
-    start_date { Date.today }
-    end_date { Date.today }
     user_id { User.first.id }
-    todo_parent_id { TodoParent.first.id }
   end
 end
