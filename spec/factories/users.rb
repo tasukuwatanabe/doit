bc563fb01f7f64faa66b06a51e6dcd04e226a9cd
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-11-16 16:11:20 +0900

    userのfactoryにpassword_confirmationカラムを追加し、rspecが通ることを確認。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     username { 'testuser' }
     email { 'testemail@example.com' }
     password { 'password' }
+    password_confirmation { 'password' }
     activated { true }
     activated_at { Time.zone.now }
   end
