dd6089c6bdfd85e041a49a4bba178317953491ee
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-11-22 16:10:45 +0900

    Authenticatorオブジェクトに関わるspecファイルを修正。

--- spec/services/authenticator.rb
+++ spec/services/authenticator.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 describe Authenticator do
   describe '#authenticate' do
     example '正しいパスワードならtrueを返す' do
-      u = build(:user)
-      expect(Authenticator.new(u).authenticate('password')).to be_truthy
+      user = build(:user)
+      expect(Authenticator.new(user).authenticate('password')).to be_truthy
     end
 
     example '誤ったパスワードならfalseを返す' do
-      u = build(:user)
-      expect(Authenticator.new(u).authenticate('foobar')).to be_falsey
+      user = build(:user)
+      expect(Authenticator.new(user).authenticate('foobar')).to be_falsey
     end
 
     example 'パスワード未設定ならfalseを返す' do
-      u = build(:user)
-      expect(Authenticator.new(u).authenticate(nil)).to be_falsey
+      user = build(:user)
+      expect(Authenticator.new(user).authenticate(nil)).to be_falsey
     end
   end
 end
