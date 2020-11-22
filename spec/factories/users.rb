e636b6f9e6fd851405fdde894db7f3a49b4ac6ca
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-11-22 16:10:21 +0900

    Userに関わるspecファイルを修正し、rspecテストを追加。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    username { 'testuser' }
-    email { 'testemail@example.com' }
+    username { 'ユーザー' }
+    email { 'user@example.com' }
     password { 'password' }
     password_confirmation { 'password' }
     activated { true }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,25 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { create(:user) }
+
   describe 'passwordの保存' do
     example '文字列を与えると、password_digestは長さ60の文字列になる' do
-      user = User.new
       user.password = 'password'
       expect(user.password_digest).to be_kind_of(String)
       expect(user.password_digest.size).to eq(60)
     end
 
     example 'nilを与えると、password_digestはnilになる' do
-      user = User.new(password_digest: 'password')
       user.password = nil
       expect(user.password_digest).to be_nil
+    end
+
+    example 'passwordが空欄の場合は無効' do
+      user = build(:user, password: nil)
+      expect(user).not_to be_valid
+    end
+
+    example 'password_confirmationが空欄の場合は無効' do
+      user = build(:user, password_confirmation: nil)
+      expect(user).not_to be_valid
+    end
+
+    example 'passwordとpassword_confirmationが異なる場合は無効' do
+      user = build(:user, password: 'password', password_confirmation: 'password_confirmation')
+      expect(user).not_to be_valid
     end
   end
 
   describe '値の正規化' do
-    example 'username前後の空白を除去' do
-      user = create(:user, username: ' testuser ')
-      expect(user.username).to eq('testuser')
+    example 'username前後の半角スペースを除去' do
+      user = create(:user, username: ' ユーザー ')
+      expect(user.username).to eq('ユーザー')
+    end
+    
+    example 'username前後の全角スペースを除去' do
+      user = create(:user, username: "\u{3000}ユーザー\u{3000}")
+      expect(user.username).to eq('ユーザー')
     end
 
     example 'emailに含まれる全角英数字を半角に変換' do
@@ -34,6 +54,16 @@ RSpec.describe User, type: :model do
   end
 
   describe 'バリデーション' do
+    example 'ユーザー名が空欄の場合は無効' do
+      user = build(:user, username: nil)
+      expect(user).not_to be_valid
+    end
+
+    example 'メールアドレスが空欄の場合は無効' do
+      user = build(:user, email: nil)
+      expect(user).not_to be_valid
+    end
+
     example '@を2個含むemailは無効' do
       user = build(:user, email: 'test@@example.com')
       expect(user).not_to be_valid
