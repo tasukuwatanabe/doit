8ead89e762e25172b7349cb999c5d4386f012535
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-11-29 16:56:42 +0900

    cookieを判別する正規表現を修正。cookie.jsの関数をアロー関数に変更。

--- frontend/components/mixins/cookie.js
+++ frontend/components/mixins/cookie.js
@@ -1,19 +1,19 @@
-function cookieStatus() {
+const cookieStatus = () => {
   const cookie_user_id = document.cookie.replace(
-    /(?:(?:^|.*;\s*)user_id\s*([^;]*).*$)|^.*$/,
+    /(?:(?:^|.*;\s*)user_id\s*=\s*([^;]*).*$)|^.*$/,
     '$1'
   );
   const cookie_remember_token = document.cookie.replace(
-    /(?:(?:^|.*;\s*)remember_token\s*([^;]*).*$)|^.*$/,
+    /(?:(?:^|.*;\s*)remember_token\s*=\s*([^;]*).*$)|^.*$/,
     '$1'
   );
 
   return cookie_user_id != '' && cookie_remember_token != '';
-}
+};
 
-function deleteCookie() {
+const deleteCookie = () => {
   document.cookie = 'user_id=;';
   document.cookie = 'remember_token=;';
-}
+};
 
 export { cookieStatus, deleteCookie };
