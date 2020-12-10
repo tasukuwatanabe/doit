a2c0b075246cc9f7c2936faf364562e0f02fa550
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-12-10 21:14:53 +0900

    AWS SESに関する設定を修正。

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -28,9 +28,9 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
-  config.action_mailer.perform_caching = false
-
   config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.perform_caching = true
 
   config.action_mailer.raise_delivery_errors = true
 
