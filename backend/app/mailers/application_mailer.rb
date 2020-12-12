47facbcca43c9afaf727bebb4236003daebc0f0f
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-12-12 16:12:33 +0900

    メールのフォーマットを修正。

--- backend/app/mailers/application_mailer.rb
+++ backend/app/mailers/application_mailer.rb
@@ -1,14 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  after_action :set_envelope_from
-  
   add_template_helper(SessionsHelper)
 
-  default from: 'noreply@example.com'
+  default from: 'DoIT <noreply@doit-app.com>'
   layout 'mailer'
-
-  private
-
-  def set_envelope_from
-    mail.smtp_envelope_from = 'DoIT <noreply@example.com>'
-  end
 end
