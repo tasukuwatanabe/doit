db75d1463b92945a6345b4bbd7a044279ac0e965
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-12-24 23:28:08 +0900

    LabelテキストがSPサイズの時に小さくなりすぎないようにfont-sizeを調整。

--- frontend/components/label-list.vue
+++ frontend/components/label-list.vue
@@ -112,14 +112,10 @@ export default {
   margin-right: 50px;
   @media (max-width: 767px) {
     margin-right: 20px;
-    font-size: 14px;
+    font-size: 0.9em;
   }
   @media (max-width: 424px) {
     margin-right: 15px;
-    font-size: 13px;
-  }
-  @media (max-width: 374px) {
-    font-size: 12px;
   }
 }
 </style>
