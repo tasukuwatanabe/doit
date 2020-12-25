f03b0cca462598dc96096b4b43e65b9b0c10348d
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-12-25 13:25:39 +0900

    ファイルパスが間違っていて404エラーがでる問題を修正。 ・constants.rb内の定数末尾のスラッシュを削除。 ・image_uploader.rbのデフォルト画像設定にスラッシュを追加。 ・user-editコンポーネントでデフォルト画像判定をしている箇所を修正。

--- backend/config/initializers/constants.rb
+++ backend/config/initializers/constants.rb
@@ -1,8 +1,8 @@
 module Constants
   UPLOAD_HOST = if Rails.env.production?
-                  "https://doit-image.s3-ap-northeast-1.amazonaws.com/"
+                  "https://doit-image.s3-ap-northeast-1.amazonaws.com"
                 else
-                  "http://localhost:3000/"
+                  "http://localhost:3000"
                 end
 
   CLIENT_HOST = if Rails.env.production?
