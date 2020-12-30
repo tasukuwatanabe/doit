0d93934adf67e24e3fb5c800185921b21a1e4991
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2020-12-30 13:31:56 +0900

    specファイルをリファクタリング。

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -40,98 +40,189 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe 'passwordの保存' do
-    it '文字列を与えると、password_digestは長さ60の文字列になる' do
-      expect(user.password_digest).to be_kind_of(String)
-      expect(user.password_digest.size).to eq(60)
-    end
-
-    it 'nilを与えると、password_digestはnilになる' do
-      user.password = nil
-      expect(user.password_digest).to be_nil
-    end
-
-    it 'passwordが空欄の場合は無効' do
-      expect(build(:user, password: nil)).not_to be_valid
-    end
-
-    it 'password_confirmationが空欄の場合は無効' do
-      expect(build(:user, password_confirmation: nil)).not_to be_valid
-    end
-
-    it 'passwordとpassword_confirmationが異なる場合は無効' do
-      user = build(:user, password: 'password', password_confirmation: 'password_confirmation')
-      expect(user).not_to be_valid
-    end
-  end
-
   describe '値の正規化' do
-    it 'username前後の半角スペースを除去' do
-      user = create(:user, username: ' ユーザー ')
-      expect(user.username).to eq('ユーザー')
+
+    context 'username' do
+      subject { user.username }
+      let(:user) { create(:user, username: username)}
+
+      context '前後に半角スペースを入力' do
+        let(:username) { ' ユーザー ' }
+
+        it '半角スペースを除去' do
+          is_expected.to eq('ユーザー')
+        end
+      end
+
+      context '前後に全角スペースを入力' do
+        let(:username) { "\u{3000}ユーザー\u{3000}" }
+
+        it '全角スペースを除去' do
+          is_expected.to eq('ユーザー')
+        end
+      end
     end
 
-    it 'username前後の全角スペースを除去' do
-      user = create(:user, username: "\u{3000}ユーザー\u{3000}")
-      expect(user.username).to eq('ユーザー')
-    end
+    context 'email' do
+      subject { user.email }
+      let(:user) { create(:user, email: email)}
 
-    it 'emailに含まれる全角英数字を半角に変換' do
-      user = create(:user, email: 'ｔｅｓｔ＠ｅｘａｍｐｌｅ．ｃｏｍ')
-      expect(user.email).to eq('test@example.com')
-    end
+      context '全角英数字で入力' do
+        let(:email) { 'ｔｅｓｔ＠ｅｘａｍｐｌｅ．ｃｏｍ' }
 
-    it 'email前後の全角スペースを除去' do
-      user = create(:user, email: "\u{3000}test@example.com\u{3000}")
-      expect(user.email).to eq('test@example.com')
+        it '全角英数字を半角に変換' do
+          is_expected.to eq('test@example.com')
+        end
+      end
+
+      context '全角スペースで入力' do
+        let(:email) { "\u{3000}test@example.com\u{3000}" }
+
+        it '全角スペースを除去' do
+          is_expected.to eq('test@example.com')
+        end
+      end
+
+      context '大文字で入力' do
+        let(:email) { 'TEST＠EXAMPLE.COM' }
+
+        it '小文字に変換' do
+          is_expected.to eq('test@example.com')
+        end
+      end
     end
   end
 
   describe 'バリデーション' do
-    it 'ユーザー名が空欄の場合は無効' do
-      expect(build(:user, username: nil)).not_to be_valid
+    subject { user }
+
+    context 'username' do
+      let(:user) { build(:user, username: username) }
+
+      context '空欄の場合' do
+        let(:username) { nil }
+
+        it '無効' do
+          is_expected.not_to be_valid
+        end
+      end
     end
 
-    it 'メールアドレスが空欄の場合は無効' do
-      expect(build(:user, email: nil)).not_to be_valid
+    context 'email' do
+      let(:user) { build(:user, email: email) }
+
+      context '空欄の場合' do
+        let(:email) { nil }
+
+        it '無効' do
+          is_expected.not_to be_valid
+        end
+      end
+
+      context '@を2個含む' do
+        let(:email) { 'test@@example.com' }
+
+        it '無効' do
+          is_expected.not_to be_valid
+        end
+      end
+
+      context '大文字と小文字との区別' do
+        let(:email) { 'test@example.com' }
+
+        it '区別しない' do
+          user.save
+          expect(build(:user, email: user.email.upcase!)).not_to be_valid
+        end
+      end
+
+      context '他のユーザーと重複' do
+        let(:email) { 'duplicate@example.com' }
+
+        it '無効' do
+          user.save
+          expect(build(:user, email: "duplicate@example.com")).not_to be_valid
+        end
+      end
     end
 
-    it '@を2個含むemailは無効' do
-      expect(build(:user, email: 'test@@it.com')).not_to be_valid
+    context 'password' do
+      let(:user) { build(:user, password: password) }
+
+      context '空欄の場合' do
+        let(:password) { nil }
+
+        it '無効' do
+          is_expected.not_to be_valid
+        end
+      end
+
+      context '6文字より少ない場合' do
+        let(:password) { 'a' * 5 }
+
+        it '無効' do
+          is_expected.not_to be_valid
+        end
+      end
+
+      context '20文字より多い場合' do
+        let(:password) { 'a' * 21 }
+
+        it '無効' do
+          is_expected.not_to be_valid
+        end
+      end
     end
 
-    it '他のユーザーのemailと重複したemailは無効' do
-      user1 = create(:user)
-      expect(user1.dup).not_to be_valid
+    context 'password_confirmation' do
+      let(:user) { build(:user, password_confirmation: password_confirmation) }
+
+      context 'password一致していない場合' do
+        let(:password_confirmation) { "wrong_confirmation" }
+
+        it '無効' do
+          is_expected.not_to be_valid
+        end
+      end
     end
 
-    it 'emailの大文字と小文字を区別しない' do
-      user1 = create(:user)
-      expect(build(:user, email: user1.email.upcase)).not_to be_valid
-    end
+    context 'unconfirmed_email' do
+      context '他のユーザーのemailと重複' do
+        let(:user1) { create(:user, email: 'already@example.com') }
+        let(:user2) { build(:user, unconfirmed_email: user1.email) }
 
-    it 'passwordが空欄の場合は無効' do
-      expect(build(:user, password: nil)).not_to be_valid
+        it '無効' do
+          expect(user2).not_to be_valid
+          expect(user2.errors.messages[:unconfirmed_email]).to include "はすでに存在します"
+        end
+      end
     end
+  end
 
-    it 'password_confirmationがpasswordと一致していない場合は無効' do
-      user = build(:user, password: "hogehoge", password_confirmation: "fugafuga")
-      expect(user).not_to be_valid
-    end
+  describe 'データの保存' do
 
-    it 'passwordが6文字より少ない場合は無効' do
-      expect(build(:user, password: "hoge")).not_to be_valid
-    end
+    context 'password_digest' do
+      subject { user.password_digest }
+      let(:user) { build(:user, password: password) }
 
-    it 'passwordが20文字より多い場合は無効' do
-      expect(build(:user, password: "hogehogehogehogehogehoge")).not_to be_valid
-    end
+      context 'passwordに文字列を与える' do
+        let(:password) { "password" }
 
-    it '未確認 email は他のユーザーの email と重複しない' do
-      create(:user, email: 'already@example.com')
-      user = build(:user, unconfirmed_email: 'already@example.com')
-      expect(user).not_to be_valid
-      expect(user.errors.messages[:unconfirmed_email]).to include "はすでに存在します"
+        it '長さ60の文字列になる' do
+          user.save
+          is_expected.to be_kind_of(String)
+          expect(user.password_digest.size).to eq(60)
+        end
+      end
+
+      context 'passwordにnilを与えると' do
+        let(:password) { nil }
+
+        it 'nilになる' do
+          user.save
+          is_expected.to be_nil
+        end
+      end
     end
   end
 end
