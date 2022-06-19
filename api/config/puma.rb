52d4d71c45c54292ef11b27571586a79bd738703
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-06-19 12:10:08 +0900

    postgresをsqliteに戻す

--- api/config/puma.rb
+++
@@ -1,7 +0,0 @@
-app_path = File.expand_path("..", __dir__)
-directory app_path
-pidfile "#{app_path}/tmp/pids/puma.pid"
-state_path "#{app_path}/tmp/pids/puma.state"
-threads 0, 16
-bind "unix://#{app_path}/tmp/sockets/puma.sock"
-activate_control_app
