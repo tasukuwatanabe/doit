7c8ac1980243071e6ba686d7095f28c8e77dc9a3
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-06-19 15:37:33 +0900

    frontendを別リポジトリへ分離

--- front/store/modules/index.js
+++
@@ -1,9 +0,0 @@
-import todo from './todo';
-import shortcut from './shortcut';
-import label from './label';
-import user from './user';
-import date from './date';
-import slideMenu from './slide_menu';
-import loading from './loading';
-
-export { todo, shortcut, label, user, date, slideMenu, loading };
