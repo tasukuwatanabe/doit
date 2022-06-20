37efd83653a1adf4798470438f3ac07e0dc29dd7
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-06-20 09:01:21 +0900

    carrierwave設定をコメントアウト

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,14 @@ require_relative "constants"
 CarrierWave.configure do |config|
   config.asset_host = Constants::UPLOAD_HOST
 
-  if Rails.env.production?
-    config.fog_directory = ENV['S3_DIRECTORY']
-    config.cache_storage = :fog
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_KEY_ID'],
-      region: 'ap-northeast-1'
-    }
-  end
+  # if Rails.env.production?
+  #   config.fog_directory = ENV['S3_DIRECTORY']
+  #   config.cache_storage = :fog
+  #   config.fog_credentials = {
+  #     provider: 'AWS',
+  #     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  #     aws_secret_access_key: ENV['AWS_SECRET_KEY_ID'],
+  #     region: 'ap-northeast-1'
+  #   }
+  # end
 end
