bf5f72205d51bae1e34e59dc7367833b6b8e82cf
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2023-12-16 13:49:00 +0900

    定数の定義を改善

--- backend/config/initializers/constants.rb
+++ backend/config/initializers/constants.rb
@@ -1,11 +1,11 @@
 module Constants
-  UPLOAD_HOST = if Rails.env.production?
+  UPLOAD_HOST ||= if Rails.env.production?
                   "https://doit-image.s3-ap-northeast-1.amazonaws.com"
                 else
                   "http://localhost:3000"
                 end
 
-  CLIENT_HOST = if Rails.env.production?
+  CLIENT_HOST ||= if Rails.env.production?
                   "https://twatanabe-doit-backend.herokuapp.com"
                 elsif Rails.env.test?
                   "http://www.example.com"
