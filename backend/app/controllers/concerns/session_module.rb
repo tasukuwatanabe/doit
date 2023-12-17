76b9153bb69afc16bc0db735741076905a295919
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2023-12-17 16:22:24 +0900

    setcookie時にdomainを設定

--- backend/app/controllers/concerns/session_module.rb
+++ backend/app/controllers/concerns/session_module.rb
@@ -4,12 +4,18 @@ module SessionModule
   def log_in(user)
     return unless user.activated?
     user.remember!
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
+    cookies.permanent.signed[:user_id] = {
+      value: user.id,
+      domain: 'doit-plan.com'
+    }
+    cookies.permanent[:remember_token] = {
+      value: user.remember_token,
+      domain: 'doit-plan.com'
+    }
   end
 
   def log_out
-    cookies.delete(:user_id) if cookies[:user_id]
-    cookies.delete(:remember_token)
+    cookies.delete(:user_id, domain: 'doit-plan.com') if cookies[:user_id]
+    cookies.delete(:remember_token, domain: 'doit-plan.com')
   end
 end
